d6e006dd8a4a2a060b198f40ba862a5360d90bae
Arie <arie@wego.com>
2014-01-16 20:09:57 +0700

    add close button

--- Blinc/app/view/register/blincRegisterViewController.m
+++ Blinc/app/view/register/blincRegisterViewController.m
@@ -28,9 +28,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	UIButton *close = [UIButton buttonWithType:UIButtonTypeCustom];
+	[close setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+	UIEdgeInsets buttonEdges = UIEdgeInsetsMake(0, 0, 0, 0);
+	[close setImageEdgeInsets:buttonEdges];
+	[close addTarget:self action:@selector(close:) forControlEvents:UIControlEventTouchUpInside];
+	[close setFrame:CGRectMake(0, 0, 40, 40)];
+	[[self navigationItem] setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:close]];
 	// Do any additional setup after loading the view.
 }
-
+-(void)close:(id)sender{
+	[self.navigationController dismissViewControllerAnimated:YES completion:Nil];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
